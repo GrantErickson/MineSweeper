3f9511d47e414d0ab3975a743f536d0bfe83e4f9
Grant Erickson <grant@granterickson.com>
2022-02-10 22:57:14 -0800

    Initial check in after 1 hour.

---
+++ MineSweeper/Game.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+    public class Game
+    {
+        // Board
+        // -1 = mine
+        // 1-8 numer of adjacent mines
+
+        // Player
+        // -1 Marked as a mine
+        // 0 Unexplored
+        // 1 Explored
+        // 2 Found a mine
+
+        public int Width { get; }
+        public int Height { get; }
+        public int Mines { get; }
+
+        public int[,] Board { get; }
+        public int[,] Player { get; }
+        public enum GameStates
+        {
+            Active,
+            Won,
+            Lost
+        }
+
+        public GameStates GameState { get; internal set; } = GameStates.Active;
+
+        // List of directions
+        private List<(int x, int y)> Directions = new()
+        {
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        };
+
+        public Game(int width, int height, int mines)
+        {
+            Width = width;
+            Height = height;
+            Mines = mines;
+
+            Board = new int[width, height];
+            Player = new int[width, height];
+            SetMines();
+            CalculateNumbers();
+        }
+
+        private void SetMines()
+        {
+            var minesToPlace = Mines;
+            var random = new Random();
+            do
+            {
+                long x = random.NextInt64(Width);
+                long y = random.NextInt64(Height);
+                if (Board[x, y] == 0)
+                {
+                    Board[x, y] = -1;
+                    minesToPlace--;
+                }
+            } while (minesToPlace > 0);
+        }
+
+        private void CalculateNumbers()
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    if (Board[x, y] == -1)
+                    {
+                        foreach (var direction in Directions)
+                        {
+                            int newX = x + direction.x;
+                            int newY = y + direction.y;
+                            if (newX >= 0 && newX < Width && y + newY >= 0 && newY < Height)
+                            {
+                                if (Board[newX, newY] != -1) Board[newX, newY]++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    if (Player[x, y] == -1) Console.Write("* ");
+                    if (Player[x, y] == 0) Console.Write(". ");
+                    if (Player[x, y] == 1) Console.Write($"{Board[x, y]} ");
+                    if (Player[x, y] == 2) Console.Write("X ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Move(int xPlay, int yPlay)
+        {
+            if (xPlay > 0 && yPlay > 0 && xPlay <= Width && yPlay <= Height)
+            {
+                int x = xPlay - 1;
+                int y = yPlay - 1;
+                if (Player[x, y] == 1) return; // Marked already
+                if (Player[x, y] == -1) return; // Explored
+
+                // If a mine, then game over.
+                if (Board[x, y] == -1)
+                {
+                    Player[x, y] = 2;
+                    GameState = GameStates.Lost;
+                    return;
+                }
+
+                Player[x, y] = 1;
+                if (Board[x, y] == 0)
+                {
+                    ExposeBoard(x, y);
+                }
+                TestForWin();
+            }
+        }
+
+        public void MarkMine(int xPlay, int yPlay)
+        {
+            if (xPlay > 0 && yPlay > 0 && xPlay <= Width && yPlay <= Height)
+            {
+                int x = xPlay - 1;
+                int y = yPlay - 1;
+                if (Player[x, y] == 0)
+                {
+                    Player[x, y] = -1;
+                }
+                else
+                {
+                    Player[x, y] = 0;
+                }
+            }
+        }
+
+        private void TestForWin()
+        {
+            int exploredCount = 0;
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    if (Player[x, y] == 1) exploredCount++;
+                }
+            }
+            if (exploredCount == Width * Height - Mines)
+            {
+                GameState = GameStates.Won;
+            }
+        }
+
+        /// <summary>
+        /// Call this with when a cell is 0 mines nearby to expose adjacent cells recursively.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        private void ExposeBoard(int x, int y)
+        {
+            foreach (var direction in Directions)
+            {
+                int newX = x + direction.x;
+                int newY = y + direction.y;
+                if (newX >= 0 && newX < Width && y + newY >= 0 && newY < Height)
+                {
+                    if (Player[newX, newY] == 0) // Unexplored
+                    {
+                        Player[newX, newY] = 1; // Mark as Explored
+                        if (Board[newX, newY] == 0)
+                        {
+                            ExposeBoard(newX, newY);
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ MineSweeper/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MineSweeper;
+
+Console.WriteLine("Welcome to Mine Sweeper!");
+
+var game = new Game(20, 10, 20);
+
+
+do
+{
+    game.Draw();
+    Console.WriteLine("Enter move x,y or *x,y to mark a mine");
+    var input = Console.ReadLine();
+    if (input != null)
+    {
+        string raw = input.Replace("*", "");
+        var chars = raw.Split(',', StringSplitOptions.RemoveEmptyEntries & StringSplitOptions.TrimEntries);
+        if (chars.Length == 2)
+        {
+            int x, y;
+            if (int.TryParse(chars[0], out x) && int.TryParse(chars[1], out y))
+            {
+                if (input.StartsWith("*"))
+                {
+                    game.MarkMine(x, y);
+                }
+                else
+                {
+                    game.Move(x, y);
+                }
+            }
+        }
+    } 
+    Console.WriteLine();
+} while (game.GameState == Game.GameStates.Active);
+
+
+game.Draw();
+if (game.GameState == Game.GameStates.Won) Console.WriteLine("You Win!");
+if (game.GameState == Game.GameStates.Lost) Console.WriteLine("Sorry, you found a mine");
+
+
